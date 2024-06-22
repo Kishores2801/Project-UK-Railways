7ecf89a331cc3ea2d2a0a1c30d62d6e48d05c4d6
Kishore <87652286+Kishores2801@users.noreply.github.com>
2024-06-22 10:35:23 +0530

    Adding files and updating readme

--- Dashboard App/app.py
+++ Dashboard App/app.py
@@ -78,5 +78,7 @@ def set_arrival_options(dep_location):
 
 # Run the app
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 8080))
-    app.run_server(debug=False, host="0.0.0.0",port=port)
+    app.run_server()
+
+
+
